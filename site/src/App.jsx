b46c8b19625514c37b5d9ef791af276cd79c46f7
Luuk de Bruin <luukdebruin@Luuks-MBP.fritz.box>
2021-11-14 12:00:11 +0100

    loading state

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import { useFetch } from './hooks/useFetch'
 import Create from './components/Create'
 import Map from './components/Map'
 
-export default class App extends React.Component {
-  render() {
+const App = () => {
+    const { isLoading, response, error } = useFetch("http://localhost:4444/api/shops/all", {})
+    if (error) {
+      return (
+        <div className="w-full h-screen flex justify-center items-center">
+          <h2>{error.message}</h2>
+        </div>
+      )
+    }
+    if (isLoading) {
+        return (
+          <div className="w-full h-screen flex justify-center items-center flex-col">
+            <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900 mb-4"></div>
+            <h2>Loading...</h2>
+          </div>
+        )
+    }
+    if (!response) {
+        return null
+    }
     return (
       <div className="w-full h-screen relative">
           <Create />
-          <Map />
+          <Map data={response}/>
       </div>
     )
-  }
 }
+
+export default App
--- site/src/components/Create.jsx
+++ site/src/components/Create.jsx
@@ -2,37 +2,42 @@ import React, { useState } from "react"
 
 const Create = () => {
     const [openModal, setOpenModal] = useState(false)
+    const [submitForm, setSubmitForm] = useState({ name: undefined, street: undefined, lat: undefined, long: undefined })
+
+    const postRequest = () => {
+        console.log(submitForm)
+    }
 
     return (
         openModal ? (
-            <div className="absolute w-screen h-full bg-gray-300 flex z-10 justify-center items-center">
+            <div className="absolute w-screen h-full bg-gray-300 bg-opacity-50 flex z-10 justify-center items-center">
                 <div className="p-4 w-96 rounded-lg bg-white">
                     <div className="flex flex-col justify-between">
                         <div className="pb-8 flex flex-none flex-row justify-between">
                             <h2 className="font-bold">Create Späti</h2>
                             <div className="cursor-pointer" onClick={() => setOpenModal(false)}>X</div>
                         </div>
-                        <form>
+                        <div>
                             <div className="pb-4">
                                 <label className="pb-2 block">Name</label>
-                                <input className="block" placeholder="Name"></input>
+                                <input className="block" type="text" placeholder="Name" onChange={e => setSubmitForm({ ...submitForm, name: e.target.value})}></input>
                             </div>
                             <div className="pb-4">
                                 <label className="pb-2 block">Street</label>
-                                <input className="block" placeholder="Street"></input>
+                                <input className="block" type="text" placeholder="Street" onChange={e => setSubmitForm({ ...submitForm, street: e.target.value})}></input>
                             </div>
                             <div className="pb-4">
                                 <label className="pb-2 block">Lat</label>
-                                <input className="block" placeholder="Lat"></input>
+                                <input className="block" type="number" placeholder="Lat" onChange={e => setSubmitForm({ ...submitForm, lat: e.target.value})}></input>
                             </div>
                             <div className="pb-4">
                                 <label className="pb-2 block">Long</label>
-                                <input className="block" placeholder="Long"></input>
+                                <input className="block" type="number" placeholder="Long" onChange={e => setSubmitForm({ ...submitForm, long: e.target.value})}></input>
                             </div>
                             <div className="pb-4">
-                                <input type="submit"></input>
+                                <button onClick={postRequest}>Submit</button>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
